3b81690794fffde294eb4a411e0d2afdea0af041
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-11-24 16:53:12 +0200

    feat: initial hook between server and editor

--- packages/editor/src/components/Toolbar/index.tsx
+++ packages/editor/src/components/Toolbar/index.tsx
@@ -1,8 +1,10 @@
 import {editorState, snippetSelector} from "../../atoms";
-import React from "react";
+import React, {useRef} from "react";
 import {useRecoilState, useRecoilValue} from "recoil";
-import {Col, Divider, Input, Row, Select, Switch} from "antd";
+import {Button, Col, Divider, Input, Row, Select, Switch} from "antd";
 import snippets from "../../snippets";
+import axios from "axios";
+// import axios from "axios";
 
 const {Option} = Select;
 
@@ -16,8 +18,21 @@ const Toolbar = () => {
 
     const snippet = useRecoilValue(snippetSelector);
 
+    const runCode = async () => {
+        const url = ref.current.state.value
+        console.log(url);
+        const {data} = await axios.post(url, {script: snippet.value});
+        alert(JSON.stringify(data))
+    }
+
+    const ref = useRef<any>()
+
     return <div>
-        <Input addonBefore="Cloud Run Endpoint" placeholder=" "/>
+        <Input ref={ref}
+               addonBefore="Cloud Run Endpoint"
+               placeholder=" "
+               defaultValue="http://localhost:8080/eval"/>
+
         <Divider orientation="center"/>
         <Row gutter={16}>
             <Col className="gutter-row" span={3}>
@@ -26,7 +41,6 @@ const Toolbar = () => {
                         checkedChildren="Dark" unCheckedChildren="Light"/>
             </Col>
             <Col className="gutter-row" span={3}>
-
                 <Select
                     showSearch
                     style={{width: 200}}
@@ -37,6 +51,9 @@ const Toolbar = () => {
                 >
                     {snippets.map(s => <Option key={s.language} value={s.name}>{s.name}</Option>)}
                 </Select>
+            </Col>
+            <Col className="gutter-row" span={10}>
+                <Button onClick={runCode}>Run Code</Button>
             </Col>
 
         </Row>

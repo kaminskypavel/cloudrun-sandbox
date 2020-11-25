fc5d83a5b619813b91c2e1ab6613366a4934438f
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-11-25 14:11:27 +0200

    feat: more examples

--- packages/editor/src/snippets/index.ts
+++ packages/editor/src/snippets/index.ts
@@ -6,9 +6,32 @@ const snippets: Snippet[] = [
         value: `console.log("hello")`
     },
     {
+        name: "javascript - spawn_process",
+        language: "javascript",
+        value: `const {execSync} = require('child_process')\n        
+const data = execSync("ls") .toString();
+console.log(data);`
+    },
+    {
         name: "python - simple",
         language: "python",
         value: `print("hello")`
+    },
+    {
+        name: "python - short sleep",
+        language: "python",
+        value: `import time\n
+time.sleep(2)
+print("Printed after 2 seconds.")
+`
+    },
+    {
+        name: "python - too long sleep",
+        language: "python",
+        value: `import time\n
+time.sleep(20*1000)
+print("this will never be printed")
+`
     },
 
 ]

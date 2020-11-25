9c0de2ea2f411d79182ba0d85e68b2e401c4c218
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-11-25 16:36:31 +0200

    fix: remove whats left from recoil

--- packages/editor/src/components/App.tsx
+++ packages/editor/src/components/App.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import PageLayout from "./PageLayout"
 
 import "./App.css";
-import {RecoilRoot} from "recoil";
 import {StoreContext} from '../context';
 import store from '../store';
 
 const App = () =>
-    <RecoilRoot>
-        <StoreContext.Provider value={store}>
-            <PageLayout/>
-        </StoreContext.Provider>
-    </RecoilRoot>
+    <StoreContext.Provider value={store}>
+        <PageLayout/>
+    </StoreContext.Provider>
 
 export default App;
